81b7fd3351f22b310772ed56161de7b3061b75b1
Serhii Komarychev <komarichev@gmail.com>
2019-10-08 14:07:01 +0300

    add social links

--- client/html.js
+++ client/html.js
@@ -8,6 +8,12 @@ const Html = ({
       <meta charset="UTF-8">
       <link rel="stylesheet" type="text/css" href="/css/main.css" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel = "stylesheet"
+      href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin = "anonymous" />
+      <link rel = "stylesheet"
+      href = "http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css" />
     </head>
     <body>
       <div id="root">${body}</div>
