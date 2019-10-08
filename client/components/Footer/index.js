44a64961a8b546851bfcd98c0436401e1f92079b
Serhii Komarychev <komarichev@gmail.com>
2019-10-08 14:44:29 +0300

    add social icons

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
 import './style.css'
-import {
-  FontAwesomeIcon
-} from '@fortawesome/react-fontawesome'
 
 
 function Footer() {
