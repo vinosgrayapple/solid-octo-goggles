78318bb20f78e433c1a96674d122e899959ea359
Serhii Komarychev <komarichev@gmail.com>
2019-10-08 13:29:23 +0300

    add social links

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -49,8 +49,6 @@ padding: 80px 15px;
       <ul>
         <li><a href="https://twitter.com/s_komarychev"><i className="fa fa-twitter-square" /></a></li>
         <li><a href="https://www.facebook.com/vinosgray"><i className="fa fa-facebook-square" /></a></li>
-        /* <li><a href="#"><i className="fa fa-snapchat-square" /></a></li>
-        <li><a href="#"><i className="fa fa-pinterest-square" /></a></li> */
         <li><a href="https://github.com/vinosgrayapple"><i className="fa fa-github-square" /></a></li>
       </ul>
       <p>Made by <a href="http://tutorialzine.com/" target="_blank" rel="noopener noreferrer">tutorialzine</a>. images courtesy to <a href="http://unsplash.com/" target="_blank" rel="noopener noreferrer">unsplash</a>.</p>
